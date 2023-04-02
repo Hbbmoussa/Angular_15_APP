b0b471cc8d8fc41aa5806cc513e3ddc926d61899
Habibou Moussa <habibou997@gmail.com>
2023-04-02 13:24:55 +0000

    add & edit dialog form function add on button add-employee

--- crud-app/src/app/app.component.ts
+++ crud-app/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EmployeeAddEditComponent } from './employee-add-edit/employee-add-edit.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'crud-app';
+
+  constructor(private _dialog: MatDialog) {}
+
+  openAddEditEmployeForm() {
+    this._dialog.open(EmployeeAddEditComponent);
+  }
 }
--- crud-app/src/app/app.module.ts
+++ crud-app/src/app/app.module.ts
@@ -7,9 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { EmployeeAddEditComponent } from './employee-add-edit/employee-add-edit.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, EmployeeAddEditComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -17,6 +19,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
