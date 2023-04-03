a1d86a06003cd1bb5d7177a9b75faa9758329a96
Habibou Moussa <habibou997@gmail.com>
2023-04-03 13:48:28 +0000

    getEmployeeList function work with res in console log

--- crud-app/src/app/app.component.ts
+++ crud-app/src/app/app.component.ts
@@ -1,18 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EmployeeAddEditComponent } from './employee-add-edit/employee-add-edit.component';
+import { EmployeeService } from './services/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crud-app';
 
-  constructor(private _dialog: MatDialog) {}
+  constructor(
+    private _dialog: MatDialog,
+    private _employeeService: EmployeeService
+  ) {}
+  ngOnInit(): void {
+    this.getEmployeeList();
+  }
 
   openAddEditEmployeForm() {
     this._dialog.open(EmployeeAddEditComponent);
+  }
+  getEmployeeList() {
+    this._employeeService.getEmployeeList().subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
